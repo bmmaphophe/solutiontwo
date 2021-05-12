7ebe8761d4e2df91c51612cdc6b63ecb4e7374b5
Brook Maphophe <bmphophe@sars.gov.za>
2021-05-12 02:31:36 +0200

    updates

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -5,8 +5,8 @@ import { Person } from '../models/person';
   selector: 'app-child',
   template: `
   <div class="container"><div class="row"><div class="card card-body" style="width: 18rem;"><div *ngFor="let person of personList"><div class="card-title"><span> FirstName: {{person.firstName}}</span></div>
-  <div class="card-title"><span> LastName: {{person.lastName}}</span></div>
-  <div class="card-title"><span> Age: {{person.age}}</span></div>
+  <div class="card-title"[hidden]="person.lastName === 'Moshe'"><span> LastName: {{person.lastName}}</span></div>
+  <div class="card-title" *ngIf="person.age !==35"><span> Age: {{person.age}}</span></div>
   <div> </div></div><button  class="btn btn-primary" (click)="SendInformationToTheParent()">Click Child</button></div></div></div>
   `,
   styleUrls: ['./child.component.scss']
